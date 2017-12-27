b2ba3ed05d4df975b7b0ac39094c7f42d2625159
JoshyPHP <s9e.dev@gmail.com>
2017-12-27 13:20:01 +0100

    Updated for Flarum Beta 8

--- bootstrap.php
+++ bootstrap.php
@@ -2,20 +2,19 @@
 
 /**
 * @package   s9e\autoimage
-* @copyright Copyright (c) 2015-2016 The s9e Authors
+* @copyright Copyright (c) 2015-2017 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\Flarum\Autoimage;
 
-use Flarum\Event\ConfigureFormatter;
+use Flarum\Formatter\Event\Configuring;
 use Illuminate\Events\Dispatcher;
-use s9e\TextFormatter\Configurator\Bundles\MediaPack;
 
 function subscribe(Dispatcher $events)
 {
 	$events->listen(
-		ConfigureFormatter::class,
-		function (ConfigureFormatter $event)
+		Configuring::class,
+		function (Configuring $event)
 		{
 			$event->configurator->Autoimage;
 		}
